0503639b5d9329a539b9400de1a6e5bf251a5fe0
Ayaz Uddin <ayaz@crossterra.com>
2026-01-17 11:55:11 -0500

    update

--- components/background-animation/index.tsx
+++ components/background-animation/index.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function BackgroundAnimation() {
   return (
-    <div className="fixed inset-0 -z-10 overflow-hidden pointer-events-none">
+    <div className="fixed inset-0 -z-10 overflow-hidden pointer-events-none bg-gradient-animation">
       <div className="wave"></div>
       <div className="wave"></div>
       <div className="wave"></div>
